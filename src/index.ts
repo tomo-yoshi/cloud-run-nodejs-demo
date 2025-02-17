e394fe945946c5f875d6a7074a898682208d97a6
tomo-yoshi <lucky.paddy10@gmail.com>
2025-02-16 22:46:09 -0800

    Create a simple node.js server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the simple Node.js server!!' });
+});
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
