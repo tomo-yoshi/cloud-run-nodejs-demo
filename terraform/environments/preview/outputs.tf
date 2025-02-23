2376ab0ddbcbfce49c4630a6a94f916c5578713d
tomo-yoshi <lucky.paddy10@gmail.com>
2025-02-22 22:46:35 -0800

    Delete Terraform, and update GH actions instead

--- terraform/environments/preview/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_url" {
-  value       = module.cloud_run.service_url
-  description = "The URL of the deployed preview environment"
-} 
--- terraform/modules/cloud-run/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_url" {
-  value       = google_cloud_run_service.service.status[0].url
-  description = "The URL of the deployed Cloud Run service"
-} 
